f38e0f1f79f4d621e4d9d5393e1c122a7fe3f469
Marcin <markowitek@gmail.com>
2017-05-05 22:31:14 +0200

    add main class

---
+++ src/main/java/org/receiptsServerApplication/ReceiptsServerApplication.java
@@ -0,0 +1,15 @@
+package org.receiptsServerApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Marcin on 05.05.2017.
+ */
+@SpringBootApplication
+public class ReceiptsServerApplication {
+
+    public static void main(String args[]){
+        SpringApplication.run(ReceiptsServerApplication.class, args);
+    }
+}
