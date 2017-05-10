653d12fc4be5aafa953cda752c6e71e13a09d9f4
Marcin <markowitek@gmail.com>
2017-05-10 19:07:01 +0200

    add conf files

--- src/main/java/org/receiptsServerApplication/ReceiptsServerApplication.java
+++ src/main/java/org/receiptsServerApplication/ReceiptsServerApplication.java
@@ -1,14 +1,15 @@
+
+/*
+ * Copyright (c) Marcin Witek 2017
+ */
+
 package org.receiptsServerApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by Marcin on 05.05.2017.
- */
 @SpringBootApplication
 public class ReceiptsServerApplication {
-
     public static void main(String args[]){
         SpringApplication.run(ReceiptsServerApplication.class, args);
     }
