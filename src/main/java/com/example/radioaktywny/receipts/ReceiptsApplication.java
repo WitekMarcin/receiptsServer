826688a638c07dd10226c04b1e38e1b9ed8b4d20
Marcin <markowitek@gmail.com>
2018-04-20 21:35:45 +0200

    migrate to gradle

---
+++ src/main/java/com/example/radioaktywny/receipts/ReceiptsApplication.java
@@ -0,0 +1,12 @@
+package com.example.radioaktywny.receipts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptsApplication.class, args);
+	}
+}
